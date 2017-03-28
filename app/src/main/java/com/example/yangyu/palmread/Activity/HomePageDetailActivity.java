3350468414bed1235eec2da50dc6418b74258d3c
yangyu <yangyu@yangyudeMacBook-Pro.local>
2017-03-28 13:50:03 +0800

    添加了水波纹点击效果；优化代码

--- app/src/main/java/com/example/yangyu/palmread/Activity/HomePageDetailActivity.java
+++ app/src/main/java/com/example/yangyu/palmread/Activity/HomePageDetailActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
@@ -22,7 +24,7 @@ import com.example.yangyu.palmread.View.HomePageDetaiBottom;
  * Created by yangyu on 2017/1/16.
  */
 
-public class HomePageDetailActivity extends AppCompatActivity implements View.OnClickListener{
+public class HomePageDetailActivity extends AppCompatActivity implements View.OnClickListener {
 
     private GetHomePageresult.PageData mData;
     private WebView mWebView;
@@ -33,54 +35,85 @@ public class HomePageDetailActivity extends AppCompatActivity implements View.On
     private String editorName;
     private ProgressBar mProgressBar;
     private HomePageDetaiBottom mDetailBottom;
+    private View mErrorView;
     private boolean isFavorite;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage_detail);
-        Intent intent=getIntent();
+        Intent intent = getIntent();
         mData = intent.getParcelableExtra(ProjectContent.EXTRA_HOME_FRAGMENT_RESULT);
         setData(mData);
-        mWebView = (WebView)findViewById(R.id.news_web);
+        mWebView = (WebView) findViewById(R.id.news_web);
         mDetailBottom = (HomePageDetaiBottom) findViewById(R.id.detail_bottom);
-        mProgressBar = (ProgressBar)findViewById(R.id.progress);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress);
+        mErrorView = findViewById(R.id.online_error_btn_retry);
         mTopBar = findViewById(R.id.top_bar);
-        mBack = (ImageView)mTopBar.findViewById(R.id.back);
-        mEditor = (TextView)mTopBar.findViewById(R.id.editor);
+        mBack = (ImageView) mTopBar.findViewById(R.id.back);
+        mEditor = (TextView) mTopBar.findViewById(R.id.editor);
         mWebView.loadUrl(detail_url);
         mWebView.setWebViewClient(mClient);
         mWebView.setWebChromeClient(mChromeClient);
         mBack.setOnClickListener(this);
         mDetailBottom.setmData(mData);
-        isFavorite= NewsCollectDbUtils.isFavoriteNews(this,mData);
+        isFavorite = NewsCollectDbUtils.isFavoriteNews(this, mData);
         mDetailBottom.setFavorite(isFavorite);
     }
-    private WebViewClient mClient=new WebViewClient(){
+
+    private WebViewClient mClient = new WebViewClient() {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
         }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+            showErrorPage();
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
     };
-    private WebChromeClient mChromeClient=new WebChromeClient(){
+    private WebChromeClient mChromeClient = new WebChromeClient() {
         @Override
         public void onProgressChanged(WebView view, int newProgress) {
             super.onProgressChanged(view, newProgress);
-            if(newProgress==100){
+            if (newProgress == 100) {
                 mProgressBar.setVisibility(View.GONE);
-            }else {
+            } else {
                 mProgressBar.setVisibility(View.VISIBLE);
                 mProgressBar.setProgress(newProgress);
             }
         }
     };
-    private void setData(GetHomePageresult.PageData result){
-        if(result!=null){
+
+    protected void showErrorPage() {
+        mWebView.setVisibility(View.INVISIBLE);
+        mErrorView.setVisibility(View.VISIBLE);
+        mErrorView.setOnClickListener(errorListener);
+    }
+
+    private View.OnClickListener errorListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mWebView.reload();
+            mErrorView.setVisibility(View.INVISIBLE);
+            mWebView.setVisibility(View.VISIBLE);
+        }
+    };
+
+    private void setData(GetHomePageresult.PageData result) {
+        if (result != null) {
             detail_url = result.mContent;
             editorName = result.mAutorName;
         }
     }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -89,8 +122,8 @@ public class HomePageDetailActivity extends AppCompatActivity implements View.On
 
     @Override
     public void onClick(View v) {
-        int id=v.getId();
-        switch(id){
+        int id = v.getId();
+        switch (id) {
             case R.id.back:
                 finish();
                 break;
@@ -102,12 +135,11 @@ public class HomePageDetailActivity extends AppCompatActivity implements View.On
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         // TODO 自动生成的方法存根
-        if(keyCode==KeyEvent.KEYCODE_BACK) {
-            if(mWebView.canGoBack()) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mWebView.canGoBack()) {
                 mWebView.goBack();
                 return true;
-            }
-            else {
+            } else {
                 finish();
             }
 
